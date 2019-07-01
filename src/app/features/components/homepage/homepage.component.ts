7059997c2a8985b087645b88792c23c7e5b40420
aahadaazar <aahadaazar@hotmail.com>
2019-07-01 22:38:03 +0500

    third commit

--- src/app/features/components/homepage/homepage.component.ts
+++ src/app/features/components/homepage/homepage.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'ge-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  selector: "ge-homepage",
+  templateUrl: "./homepage.component.html",
+  styleUrls: ["./homepage.component.scss"]
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  blogArray = [
+    {
+      owner : "Aahad Aazar",
+      name: "My First Blog",
+      description: "This is short description for a blog",
+      html:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+      owner : "Aazar Farooq",
+      name: "My First Blog",
+      description: "This is short description for a blog",
+      html:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    }
+  ];
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.blogArray);
   }
-
 }
