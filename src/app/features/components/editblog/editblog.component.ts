1e37896ebb059cc5cb58ba933dd74f26d295a90b
aahadaazar <aahadaazar@hotmail.com>
2019-07-02 00:56:49 +0500

    Add Blog / Edit Blog UI/UX Complete

--- src/app/features/components/editblog/editblog.component.ts
+++ src/app/features/components/editblog/editblog.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { log } from 'ng-zorro-antd';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'ge-editblog',
-  templateUrl: './editblog.component.html',
-  styleUrls: ['./editblog.component.scss']
+  selector: "ge-editblog",
+  templateUrl: "./editblog.component.html",
+  styleUrls: ["./editblog.component.scss"]
 })
 export class EditblogComponent implements OnInit {
-
-  constructor() { }
+  editBlog = {
+    name: " ",
+    description: "",
+    html: ""
+  };
+  notValidated = true;
+  constructor(private _Activatedroute: ActivatedRoute) {}
 
   ngOnInit() {
+    this._Activatedroute.paramMap.subscribe(params => {
+      console.log(params);
+      // this.editBlog = params;
+      console.log(params.params.name);
+    });
   }
 
+  saveBlog() {
+    console.log("Hello World");
+    console.log(this.newBlog);
+  }
+  checkValidation(event) {
+    if (
+      this.newBlog.name === "" ||
+      this.newBlog.description === "" ||
+      this.newBlog.html === ""
+    ) {
+      this.notValidated = true;
+    } else {
+      this.notValidated = false;
+    }
+  }
 }
